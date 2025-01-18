5b12613b5296327a7fb1b708da53d2ec148524d6
lphexter <13104592+lphexter@users.noreply.github.com>
2025-01-18 20:26:23 +0200

    updating defaults

--- src/project/config/clip_config.py
+++ src/project/config/clip_config.py
@@ -3,8 +3,8 @@
 #############################################
 SUBJECT = 1
 ROOT_DATA_DIR = "algonauts_2023_tutorial_data"
-ROI = "V1"
-REGION_CLASS = "None"
+ROI = "V1"  # options: [ "V1", "V2","V3","hV4","EBA","FBA-1","FBA-2","mTL-bodies","OFA","FFA-1","FFA-2","mTL-faces","aTL-faces","OPA","PPA","RSC","OWFA","VWFA-1","VWFA-2","mfs-words","mTL-words","early","midventral","midlateral","midparietal","ventral","lateral","parietal"]
+REGION_CLASS = "None"  # e.g., "Visual", "Bodies", "Faces", "Places", "Words", "Streams"
 DESIRED_IMAGE_NUMBER = 1000
 
 #############################################
--- src/project/config/config.py
+++ src/project/config/config.py
@@ -2,9 +2,9 @@
 # USER / EXPERIMENTAL PARAMETERS (Pipeline 1)
 #############################################
 SUBJECT = 1  # e.g., 1, 2, ... up to 8
-DESIRED_IMAGE_NUMBER = 500  # how many images to use
-ROI = "V1v"  # e.g., "V1v" or "None"
-REGION_CLASS = "None"  # e.g., "Visual", "Faces", etc.
+DESIRED_IMAGE_NUMBER = 10  # how many images to use
+ROI = "V1"  # options: [ "V1", "V2","V3","hV4","EBA","FBA-1","FBA-2","mTL-bodies","OFA","FFA-1","FFA-2","mTL-faces","aTL-faces","OPA","PPA","RSC","OWFA","VWFA-1","VWFA-2","mfs-words","mTL-words","early","midventral","midlateral","midparietal","ventral","lateral","parietal"]
+REGION_CLASS = "None"  # e.g., "Visual", "Bodies", "Faces", "Places", "Words", "Streams"
 ROOT_DATA_DIR = "algonauts_2023_tutorial_data"
 
 # RDM, model, and training parameters
@@ -16,5 +16,5 @@ NEW_WIDTH = 128
 NEW_HEIGHT = 128
 NUM_CHANNELS = 1  # 1 for grayscale
 TEST_SIZE = 0.2  # Train/test split ratio
-EPOCHS = 10
+EPOCHS = 3
 ALPHA = 0.5  # Weight for correlation vs MSE in custom loss
