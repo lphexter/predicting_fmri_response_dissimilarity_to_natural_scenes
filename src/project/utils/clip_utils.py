53824d2af0b0acac980e8f0cbf5bfecab1039f08
lphexter <13104592+lphexter@users.noreply.github.com>
2025-01-30 16:40:01 +0200

    fix embeddings issue

--- src/project/utils/clip_utils.py
+++ src/project/utils/clip_utils.py
@@ -18,8 +18,6 @@ def load_clip_model(pretrained_model_name="openai/clip-vit-base-patch32", device
 
 def get_image_embeddings(images, desired_image_number=clip_config.DESIRED_IMAGE_NUMBER, device="cpu", is_thingsvision=False):  # noqa: FBT002
     try:
-        type_embeddings = torch.Tensor
-        
         # load embeddings from a file, not from scratch
         if clip_config.LOAD_EMBEDDINGS_FILE != "":
             logger.info("Loading embeddings from local file: %s", clip_config.LOAD_EMBEDDINGS_FILE)
@@ -27,32 +25,30 @@ def get_image_embeddings(images, desired_image_number=clip_config.DESIRED_IMAGE_
             if is_thingsvision:  # process thingsvision embeddings with numpy
                 logger.info("Loading THINGSvision")
                 embeddings = np.load(clip_config.LOAD_EMBEDDINGS_FILE, allow_pickle=True).astype(np.float32)
-                type_embeddings = np.ndarray
             else:
-                embeddings = torch.from_numpy(np.load(clip_config.LOAD_EMBEDDINGS_FILE)).to(
-                    device
-                )  # Move to device (CPU/GPU) if needed
-            embeddings = embeddings[:desired_image_number]
-            logger.info("Embeddings shape: %s", embeddings.shape)
-            return embeddings
-        
-        # load from scratch with CLIP model and provided directory
-        logger.info("Loading embeddings from scratch")
-        logger.info("Loading CLIP Model: %s", clip_config.PRETRAINED_MODEL)
+                embeddings = np.load(clip_config.LOAD_EMBEDDINGS_FILE)
+            embeddings = torch.from_numpy(embeddings).to(device)[:desired_image_number] # Move to device (CPU/GPU) if needed
 
-        model, processor = load_clip_model(pretrained_model_name=clip_config.PRETRAINED_MODEL, device=device)
-        inputs = processor(images=images, return_tensors="pt", padding=True).to(device)
-        with torch.no_grad():
-            image_features = model.get_image_features(**inputs)
-            embeddings = image_features / image_features.norm(p=2, dim=-1, keepdim=True)
+        else:
+            # load from scratch with CLIP model and provided directory
+            logger.info("Loading embeddings from scratch")
+            logger.info("Loading CLIP Model: %s", clip_config.PRETRAINED_MODEL)
+
+            model, processor = load_clip_model(pretrained_model_name=clip_config.PRETRAINED_MODEL, device=device)
+            inputs = processor(images=images, return_tensors="pt", padding=True).to(device)
+            with torch.no_grad():
+                image_features = model.get_image_features(**inputs)
+                embeddings = image_features / image_features.norm(p=2, dim=-1, keepdim=True)
 
         # if our embeddings don't process correctly, raise an error and exit
-        if embeddings is None or not isinstance(embeddings, type_embeddings):
+        if embeddings is None or not isinstance(embeddings, torch.Tensor):
             raise ValueError("Failed to generate embeddings.")
+    
     # catch if load embeddings file isn't correct, ValueError as specified above
     except (FileNotFoundError, ValueError) as e:
         logger.error("Error loading CLIP embeddings: %s", e)
         sys.exit(1)
+    
     # log the shape and return the scratch-made embeddings
     logger.info("Embeddings shape: %s", embeddings.shape)
     return embeddings
